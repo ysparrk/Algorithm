65c6a7a9395b1398ee5639330871a72ab7c7daac
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-02-01 09:15:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 56 ms, Memory: 39732 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,16 +1,15 @@
 import random
                                     
 height_list = []
-# while True:
-#     height = input()
-#     if height == '':
-#         break
-#     height_list.append(height)
-
+# 1
+# input 받아서 height_list에 추가
 for i in range(9):
     height_list.append(int(input()))
 
-
+# 2
+# random.sample 이용해 랜덤으로 7개 요소 담은 리스트 출력
+# while문으로 height_sample의 합이 100이 아니면 continue로 위로 돌아가 다시 random 리스트 출력
+# height_sample의 합이 100이면 오름차순 정렬하고 하나씩 출력, + break로 종료.
 while True:
     height_sample = random.sample(height_list, 7)
     if sum(height_sample) != 100:
