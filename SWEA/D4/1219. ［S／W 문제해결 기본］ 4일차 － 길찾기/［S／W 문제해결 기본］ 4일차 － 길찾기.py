e5230327823cfddf5e4f221ec1164687fcaf27d4
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-02-14 16:41:33 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 178 ms, Memory: 59,620 KB -BaekjoonHub

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
@@ -12,22 +12,20 @@ while True:
             arr[n1][n2] = 1  # 방향 존재, (n1, n2)
 
         # 2. 탐색
-        rlt = [] # 탐색 경로 입력
-        visited = [0] * (101)  # 방문 여부 확인 list
+        visited = [0] * (100)  # 방문 여부 확인 list
 
         # 2-1
         def dfs(v):
             visited[v] = 1  # 지나친 정점은 1로 표시
-            rlt.append(v)
             # 현재 v는 시작정점, 인접한 정점 중 방문을 안한 곳
-            for w in range(1, 101):
+            for w in range(1, 100):
                 if arr[v][w] == 1 and visited[w] == 0:
                     dfs(w)  # 재귀로 정점 호출
 
         # 3
-        # 시작정점 = 0, 경로를 탐색 => 경로 중 99가 있는지 확인
+        # 시작정점 = 0, visited의 마지막 값이 1로 채워졌다면, 경로 99를 지나간 것.
         dfs(0)
-        if 99 in rlt:
+        if visited[-1] == 1:
             print(f'#{T} 1')
         else:
             print(f'#{T} 0')
