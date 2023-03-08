ff2fea61b49b36b0d706fb4af200c82783256935
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-03-08 21:18:14 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 한 줄을 읽고, int로 변경
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b / 10) % 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}
