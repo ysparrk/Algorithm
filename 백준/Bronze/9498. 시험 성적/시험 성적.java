e464804db61fe9507789259499a78bca39229065
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-03-09 21:38:28 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int score = Integer.parseInt(br.readLine());
+
+        if ((score >= 90) && (score <= 100)) {
+            bw.write("A");
+        }
+        else if (score >= 80) {
+            bw.write("B");
+        }
+        else if (score >= 70) {
+            bw.write("C");
+        }
+        else if (score >= 60) {
+            bw.write("D");
+        }
+        else {
+            bw.write("F");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
