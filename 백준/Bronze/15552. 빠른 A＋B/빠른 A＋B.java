07ded11eac4d1989bc1300b02adacd30e83fdddd
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-03-13 15:57:27 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 868 ms, Memory: 236368 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 1. input
+        int n = Integer.parseInt(br.readLine());  // tc 개수
+        
+        // 2. 계산
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(a + b + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
