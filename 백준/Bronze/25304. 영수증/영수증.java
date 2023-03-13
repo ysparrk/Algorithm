35583afc951e7a799250b7470e29ad20e82498e9
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-03-13 15:03:21 +0900

    [Bronze V] Title: 영수증, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 1. input
+        int x = Integer.parseInt(br.readLine()); // 총 금액
+        int n = Integer.parseInt(br.readLine()); // 물건 종류 수
+
+        // 2. for문
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a * b;
+        }
+
+        // 3. 결과 출력
+        if (sum == x) {
+            bw.write("Yes");
+        }
+        else {
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
