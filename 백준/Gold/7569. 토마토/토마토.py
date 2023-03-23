671016a20ff3a658f01c5a35e076ba54a6c20d71
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-03-23 21:10:23 +0900

    [Gold V] Title: 토마토, Time: 832 ms, Memory: 291876 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,13 +2,10 @@ from collections import deque
 
 # 1
 # bfs 함수
-# 토마토에 표시되는 초 > set에 추가(중복제거)
-# answer : max(set) - 1 / set에 1만 있다면 -> 이미 다 익은 것
+# 익어가는 초 표시한 tomato return
 def bfs():
-    rlt = set()  # set
     while queue:
         x, y, z = queue.popleft()
-        rlt.add(tomato[x][y][z])
         # 3차원
         dx = [-1, 1, 0, 0, 0, 0]
         dy = [0, 0, -1, 1, 0, 0]
@@ -22,8 +19,7 @@ def bfs():
             if 0 <= sx < h and 0 <= sy < n and 0 <= sz < m and tomato[sx][sy][sz] == 0:
                 queue.append([sx, sy, sz])
                 tomato[sx][sy][sz] = tomato[x][y][z] + 1  # 1 더해서 익음 표시
-                rlt.add(tomato[sx][sy][sz])  # set에 추가
-    return rlt
+    return tomato
 
 # 2
 m, n, h = map(int, input().split())  # 상자 크기, 상자 수
@@ -39,12 +35,12 @@ for i in range(h):
                 queue.append([i, j, k])
 
 # 3. 결과 출력
-rlt = bfs()
-if len(rlt) == 1:  # 다 익은 경우
+tomato = bfs()
+tomatoss = sum(sum(tomato, []), []) # 3차원 -> 1차원
+
+if 0 in tomatoss:
+    print(-1)
+elif 2 in tomatoss:  # 2가 있다면 normal한 방식
+    print(max(tomatoss) - 1)
+else:
     print(0)
-else: # 이외의 경우
-    tomatoss = sum(sum(tomato, []), []) # 3차원 배열 -> 1차원
-    if 0 in tomatoss:  # 0 존재 -> 불가능
-        print(-1)
-    else:
-        print(max(rlt) - 1)
