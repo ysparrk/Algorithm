f41e8ddbd8a6e40857d4a6b69beb765392071b32
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-06-16 00:01:50 +0900

    [level 3] Title: 징검다리 건너기, Time: 143.05 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/lv3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062#) 
 
 ### 성능 요약
 
-메모리: 18.5 MB, 시간: 305.18 ms
+메모리: 18.6 MB, 시간: 143.05 ms
 
 ### 구분
 
--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -18,17 +18,15 @@ def check(stones, s, k):
 
 # 2. binary search
 def solution(stones, k):
-    start = 0
+    start = 1  # 최소 한 명은 건널 수 있음
     end = max(stones) + 1
     
+    # start와 end가 하나 차이나면 return
     while start < end - 1:
         mid = (start + end) // 2
         if check(stones, mid, k):
-            start = mid
+            start = mid  # True -> start change
         else:
             end = mid
   
     return start
-        
-
-
