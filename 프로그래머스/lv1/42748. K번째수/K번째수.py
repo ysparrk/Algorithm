abd7722f8353dc26380b1722826c6260511b2de7
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-06-17 23:46:43 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in commands:
+        ary = array[i[0] - 1: i[1]]   
+        ary.sort()   
+        answer.append(ary[i[2] - 1])
+        
+    return answer
