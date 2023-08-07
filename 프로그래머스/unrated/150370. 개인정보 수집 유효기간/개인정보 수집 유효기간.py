625ee916fa5b9e37899499b5db5920444b3479a9
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-08-08 00:33:16 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,34 +1,22 @@
+# 1. 날짜를 dates로 환산
+def time_convert(t) :
+    year, month, date = map(int, t.split('.'))
+    return (year * 12 * 28) + (month * 28) + date
+
+# 2. 결과
 def solution(today, terms, privacies):
-    answer = []
-    time_dict = dict()
-    year, month, day = int(today[0:4]), int(today[5:7]), int(today[8:])
+    term_dict = dict()
+    today = time_convert(today)
+    answer = []    
     
-    for term in terms : 
-        case = term[0]
-        time_dict[case] = int(term[2:])
+    for term in terms :
+        code, period = term.split()
+        term_dict[code] = int(period) * 28  # dates 계산
     
-    for i in range(len(privacies)):
-        date, case = privacies[i].split()
-        p_year, p_month, p_day = int(privacies[i][0:4]), int(privacies[i][5:7]), int(privacies[i][8:10])
-        
-        p_month += time_dict[case]
-        
-        while p_month > 12 : 
-            p_month -= 12
-            p_year +=1
-            
-            
-        if p_year > year :
-            continue
-            
-        elif p_year == year :
-            if p_month > month :
-                continue
-                
-            elif p_month == month :
-                if p_day > day :
-                    continue
-                    
-        answer.append(i+1)
+    for idx, privacy in enumerate(privacies) :
+        start, code = privacy.split()
+        end = time_convert(start) + term_dict[code]
+        if end <= today :
+            answer.append(idx+1)    
     
     return answer
