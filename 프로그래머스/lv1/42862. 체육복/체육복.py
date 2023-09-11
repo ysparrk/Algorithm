47565949f98a755a408d4781f367b49eb588b1d9
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-09-11 23:03:17 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,19 @@
+def solution(n, lost, reserve):
+    n -= len(lost) 
+    
+    lost.sort()
+    reserve.sort()
+    
+    # 도난당한 사람 == 여벌 -> 이거 먼저 제거
+    for l in lost:
+        if l in reserve:
+            n += 1
+            reserve.remove(l)
+        elif (l-1) in reserve and (l-1) not in lost:
+            n += 1
+            reserve.remove(l-1)
+        elif (l+1) in reserve and (l+1) not in lost:
+            n += 1
+            reserve.remove(l+1)
+    
+    return n
