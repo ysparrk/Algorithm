f5ce96c18cec9438bcc432977971afa4d0146e43
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-12-10 00:02:44 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 94.69 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,26 +1,33 @@
-def find(parents, money, number, answer):
-    if parents[number] == number or money // 10 == 0:
-        answer[number] += money
-        return
-    send = money // 10
-    mine = money - send
-    answer[number] += mine
-    find(parents, send, parents[number], answer)
-    return
-
-
 def solution(enroll, referral, seller, amount):
-    n = len(enroll) 
-    answer = [0] * (n + 1) 
-    d = {} 
-    parents = [i for i in range(n + 1)] 
-    for i in range(n):
-        d[enroll[i]] = i + 1
-    for i in range(n):
-        if referral[i] == "-":
-            parents[i + 1] = 0
-        else:
-            parents[i + 1] = d[referral[i]]
-    for i in range(len(seller)):
-        find(parents, amount[i] * 100, d[seller[i]], answer)
-    return answer[1:]
+    p_num = len(enroll)
+    profit = [0] * (p_num)  # 이익 집계
+
+    # 1. enroll 딕셔너리 만들기
+    enroll_dict = {name: i for i, name in enumerate(enroll)}
+
+
+    # 2. 탐색
+    for j in range(len(seller)):
+        sell_idx = enroll_dict[seller[j]]  # 파는 사람
+        sell_profit = amount[j] * 100 - amount[j] * 100 // 10
+        profit[sell_idx] += sell_profit  # 내 가격
+
+        # 부모 차례대로 더해주기
+        temp = amount[j] * 100 - sell_profit # 처음 시작하는 가격
+        while True:
+            parent_name = referral[sell_idx]
+            if parent_name == '-':
+                break
+
+            else:
+                parent_idx = enroll_dict[parent_name]
+                temp_to = temp // 10  # 줄거
+                if temp_to >= 1:
+                    profit[parent_idx] += (temp - temp_to)
+                else:
+                    profit[parent_idx] += temp
+                    break
+                sell_idx = parent_idx
+                temp = temp_to
+
+    return profit
