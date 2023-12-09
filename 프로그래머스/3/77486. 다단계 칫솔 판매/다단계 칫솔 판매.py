1c2e6f0bb5c5fa973ea2c601da97ba081c5b1a17
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2023-12-09 23:36:38 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 185.10 ms, Memory: 20.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -0,0 +1,26 @@
+def find(parents, money, number, answer):
+    if parents[number] == number or money // 10 == 0:
+        answer[number] += money
+        return
+    send = money // 10
+    mine = money - send
+    answer[number] += mine
+    find(parents, send, parents[number], answer)
+    return
+
+
+def solution(enroll, referral, seller, amount):
+    n = len(enroll) 
+    answer = [0] * (n + 1) 
+    d = {} 
+    parents = [i for i in range(n + 1)] 
+    for i in range(n):
+        d[enroll[i]] = i + 1
+    for i in range(n):
+        if referral[i] == "-":
+            parents[i + 1] = 0
+        else:
+            parents[i + 1] = d[referral[i]]
+    for i in range(len(seller)):
+        find(parents, amount[i] * 100, d[seller[i]], answer)
+    return answer[1:]
