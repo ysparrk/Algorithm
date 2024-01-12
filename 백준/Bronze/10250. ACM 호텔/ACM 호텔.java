2184f9cdf8778e649857c230d11764ef5fd9933b
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2024-01-12 14:49:59 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int H = Integer.parseInt(st.nextToken());  // 층 수
+            st.nextToken(); // W 필요 없음
+            int N = Integer.parseInt(st.nextToken());  // 손님 번호
+
+            if (N % H == 0) {
+                sb.append((H * 100) + (N / H)).append('\n');
+            } else {
+                sb.append(((N % H) * 100) + ((N / H) + 1)).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
