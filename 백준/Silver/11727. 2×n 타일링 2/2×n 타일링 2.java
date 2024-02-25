2aee0ff539a2586e89f2a32d512dde8f7796ba09
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2024-02-25 13:09:03 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i < N + 1; i++) {
+            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
