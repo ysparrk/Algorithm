eb343f4a8c0f6fa0f19e089a14ac6c90271377f0
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2024-03-07 22:26:57 +0900

    [Silver IV] Title: 보물, Time: 128 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st1.nextToken());
+            B[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[N - i - 1];
+        }
+        System.out.println(sum);
+    }
+}
