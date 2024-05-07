569d749106570211a6f90255993c8395c0f33917
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2024-05-07 21:54:16 +0900

    [level 3] Title: 인사고과, Time: 159.51 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.py
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -1,23 +1,3 @@
 def solution(scores):
-    '''
-    1. 1) 근무태도 내림차순 2) 동료 평가 오름차순 정렬
-    2. 근무태도 + 동료평가 점수 비교    
-    '''
-    
-    W = scores[0]
-    W_sum = sum(W)
-    rlt = 1
-    
-    scores.sort(key=lambda x : (-x[0], x[1]))
-    tmp = 0  # 동료평가 점수
-    
-    for s in scores:
-        # 1) 인센티브 받지 못하는 경우
-        if W[0] < s[0] and W[1] < s[1]:
-            return -1
-        
-        if W_sum < sum(s) and tmp <= s[1]:
-            rlt += 1
-            tmp = s[1]
-
-    return rlt
+    answer = 0
+    return answer
