e2c8a748890f77eb9ee1bb8e74fc537cac7d5db2
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2024-07-29 15:24:17 +0900

    [Gold IV] Title: 사촌, Time: 980 ms, Memory: 162796 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.java
+++ 백준/Gold/9489. 사촌/사촌.java
@@ -20,12 +20,12 @@ public class Main {
             // 종료 조건
             if (N == 0 && K == 0) break;
 
-            idxs = new int[N + 1];
-            parent = new int[N + 1];
+            idxs = new int[N];
+            parent = new int[N];
 
             st = new StringTokenizer(br.readLine());
 
-            for (int i = 1; i <= N; i++) {
+            for (int i = 0; i < N; i++) {
                 idxs[i] = Integer.parseInt(st.nextToken());
 
                 if (idxs[i] == K) kIdx = i;
@@ -42,20 +42,19 @@ public class Main {
         int rlt = 0;
 
         // 1) parent idx 기록
-        parent[0] = -1;
-        parent[1] = 0;  //루트 노드 부모X
+        parent[0] = -1;  //루트 노드 부모X
 
-        int parentIdx = 1;
-        for (int i = 2; i <= N; i++) {
+        int parentIdx = 0;
+        for (int i = 1; i < N; i++) {
             parent[i] = parentIdx;
 
             // 연속된 순열이 아니라면
-            if (i < N && (idxs[i] + 1) != idxs[i + 1]) parentIdx++;
+            if (i < N - 1 && (idxs[i] + 1) != idxs[i + 1]) parentIdx++;
         }
 
 
         // 2) 부모는 다르지만, 부모의 부모가 같은 경우
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < N; i++) {
             if (parent[kIdx] != parent[i] && parent[parent[kIdx]] == parent[parent[i]]) rlt++;
         }
 
