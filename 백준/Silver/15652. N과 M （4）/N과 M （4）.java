46913dfdd620062d2cadf4ac70f9b2487ba3c450
ysparrk <122508507+ysparrk@users.noreply.github.com>
2024-09-19 19:56:52 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 20932 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] selected;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        selected = new int[M];
+
+        recur(0, 1);
+        System.out.println(sb);
+    }
+
+    private static void recur(int depth, int n) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = n; i <= N; i++) {
+            selected[depth] = i;
+            recur(depth + 1, i);
+        }
+    }
+}
