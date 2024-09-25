aa6001294df16775d50efc8c9d5c950ee612c7fc
ysparrk <122508507+ysparrk@users.noreply.github.com>
2024-09-25 23:20:05 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 22812 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] nums, selected;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        selected = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        recur(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void recur(int depth, int idx) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        for (int i = idx; i < N; i++) {
+            selected[depth] = nums[i];
+            recur(depth + 1, i);
+        }
+    }
+}
