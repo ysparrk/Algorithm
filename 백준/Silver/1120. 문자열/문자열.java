961faa3e9fd65effe360b6b8dea2289665e81dcb
ysparrk <122508507+ysparrk@users.noreply.github.com>
2025-02-14 23:01:53 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int rlt = 50;
+        for (int i = 0; i <= B.length() - A.length(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < A.length(); j++) {
+                if (A.charAt(j) != B.charAt(j + i)) cnt++;
+            }
+            rlt = Math.min(rlt, cnt);
+        }
+        System.out.println(rlt);
+    }
+}
