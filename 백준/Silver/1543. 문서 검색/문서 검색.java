9a67b54a46120fe867adce2113310d5fe16ab1dd
ysparrk <122508507+ysparrk@users.noreply.github.com>
2025-02-15 23:17:12 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        str1 = str1.replace(str2, "");
+        System.out.println((len1 - str1.length()) / len2);
+    }
+}
