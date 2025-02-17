8fdbf0a02d6ec5e5e1428717aa01d7d6bda0cf99
ysparrk <122508507+ysparrk@users.noreply.github.com>
2025-02-17 23:22:09 +0900

    [Silver V] Title: 소수, Time: 116 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int rlt = 0;
+
+        for(int i = 0; i < N; i++) {
+            A = (A % B) * 10;
+            rlt = A / B;
+        }
+        System.out.println(rlt);
+    }
+}
