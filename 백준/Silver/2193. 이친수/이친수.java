3f706ad66835691ec238991d8ccf2e183c4b435e
ysparrk <122508507+ysparrk@users.noreply.github.com>
2025-02-22 23:17:40 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    private static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
