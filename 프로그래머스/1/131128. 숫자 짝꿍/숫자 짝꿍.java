df274e83ce91c124cb6f02c08e7e622ec143bedc
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2026-04-19 23:25:47 +0900

    [level 1] Title: 숫자 짝꿍, Time: 79.78 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+
+        for (char c : X.toCharArray()) {
+            countX[c - '0']++;
+        }
+
+        for (char c : Y.toCharArray()) {
+            countY[c - '0']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 9; i >= 0; i--) {
+            int commonCount = Math.min(countX[i], countY[i]);
+            for (int j = 0; j < commonCount; j++) {
+                sb.append(i);
+            }
+        }
+
+        if (sb.length() == 0) {
+            return "-1";
+        }
+
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+
+        return sb.toString();
+    }
+}
