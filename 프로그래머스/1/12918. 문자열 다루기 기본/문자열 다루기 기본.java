a8299c849bcd713808d8f0fbab892f0221a723e3
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2026-06-07 19:45:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) < 48 || s.charAt(i) > 57) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
