7997982bcda5b139f26e88b56e1c914dd4932a44
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2026-08-23 11:25:32 +0900

    Time: 0 ms (100%), Space: 43.4 MB (9%) - LeetHub

---
+++ LeetCode/0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int[] isJewel = new int[128];
+      
+        for (char jewel : jewels.toCharArray()) {
+            isJewel[jewel] = 1;
+        }
+      
+        int jewelCount = 0;
+        for (char stone : stones.toCharArray()) {
+            jewelCount += isJewel[stone];
+        }
+      
+        return jewelCount;
+    }
+}
