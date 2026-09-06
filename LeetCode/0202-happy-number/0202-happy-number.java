296567313da276fc3f4555a83f433a3705377261
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2026-09-06 19:50:51 +0900

    Time: 1 ms (77.8%), Space: 42.3 MB (66.48%) - LeetHub

---
+++ LeetCode/0202-happy-number/0202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visited = new HashSet<>();
+      
+        while (n != 1 && !visited.contains(n)) {
+            visited.add(n);
+          
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+          
+            n = sum;
+        }
+      
+        return n == 1;
+    }
+}
