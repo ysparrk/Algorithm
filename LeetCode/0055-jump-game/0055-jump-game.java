a01ba2ca35b458f4a55711d7699a908189d49476
Youngseo Park <122508507+ysparrk@users.noreply.github.com>
2026-09-19 22:41:22 +0900

    Time: 3 ms (20.44%), Space: 47.7 MB (91.07%) - LeetHub

--- LeetCode/0055-jump-game/0055-jump-game.java
+++ LeetCode/0055-jump-game/0055-jump-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //첫번째 인덱스 초기에 위치 점프거리
-        int maxIdx = 0;
+
+        int maxIdx = 0;  //지금까지 도달할 수 있는 최대 인덱스
 
         for (int i = 0; i < nums.length; i++) {
             if (maxIdx < i) {
